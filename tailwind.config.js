fb3a5c64224c3dda09905e3ef1ceaac03af529e2
goshamartynovich <goshamartynovich@gmail.com>
2024-07-30 17:48:52 +0300

    feat: finishing touches of shadcn and tailwind configuration, updated eslint config to use export default

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["src/**/*.{ts,tsx,js,jsx}" ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
       container: {
         center: true,
