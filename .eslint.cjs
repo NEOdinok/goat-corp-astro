acb26b73adac47646d27dcb744c4e8964cac441a
goshamartynovich <goshamartynovich@gmail.com>
2024-08-09 01:23:18 +0300

    chore: eslint

--- .eslint.cjs
+++ .eslint.cjs
@@ -1,11 +1,21 @@
-export default {
+module.exports = {
   env: {
     browser: true,
     es2020: true,
+    node: true, // Recognize Node.js globals
   },
-  extends: [],
-  parser: "",
-  parserOptions: {},
-  plugins: [],
-  rules: {},
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:astro/recommended", // Add Astro recommended config
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["jsx-a11y", "prettier", "astro"],
+  rules: {
+    "@typescript-eslint/no-var-requires": "off", /
+  },
 };
